ae070773111ef5b8bde44e471ae1818e66f6145b
Ricardo Lopez <rlopez4@wisc.edu>
2016-03-24 00:25:43 -0500

    add initial implimentation, update readme

--- Readme.md
+++ Readme.md
@@ -1,3 +1,15 @@
 #Multi Page Router
 
-Calls functions on page load based on the path.
+Routing for non single page applications or applications that don't need history. Multi page router allows you to call a set of functions on page load using express style path strings (e.g. `/foo/:bar`).
+
+*Heavily inspired by [page.js](https://github.com/visionmedia/page.js)*
+
+##Usage
+
+```JavaScript
+multiPageRouter({
+  'foo' : [cb1, cb2],
+  'foo/:bar' : cb,
+  '*' : [cb1, cb3]
+})
+```
--- index.js
+++ index.js
@@ -3,22 +3,138 @@
 /*
  * Load dependencies
  */
- var pathtoRegexp = require('path-to-regexp');
+var pathToRegexp = require('path-to-regexp');
 
- var currentPath = '';
-
+/*
+ * Grab the current path from the window object
+ */
+var currentPath = window.location.pathname;
 
 /**
- * Configure multi page router by passing in an array
- * multiPageRouter([
- *   {'/foo' : [cb1, cb2] },
- *   {'/foo/:bar' : [cb1, cb2] },
- *   {'*' : [cb1, cb2] }
- * ])
- * @param  {Array} routeConfigs An array of route configuration objects
+ * Check if an object is an array.
+ * @param  {Object}  arg the object to check
+ * @return {Boolean}
  */
-function multiPageRouter(routeConfigs) {
-
+function isArray(arg) {
+  return Object.prototype.toString.call(arg) === '[object Array]';
 };
 
-module.expoerts = multiPageRouter;
+/**
+ * Remove URL encoding from the given `str`.
+ * Accommodates whitespace in both x-www-form-urlencoded
+ * and regular percent-encoded form.
+ *
+ * @param {string} val - URL component to decode
+ */
+function decodeURLEncodedURIComponent(val) {
+  if (typeof val !== 'string') { return val; }
+  return decodeURIComponent(val.replace(/\+/g, ' '));
+};
+
+/**
+ * Configure multi page router by passing in an array.
+ * Order is not guaranteed. TODO: potential enhancement?
+ *
+ * multiPageRouter({
+ *   'foo' : [cb1, cb2],
+ *   'foo/:bar' : cb,
+ *   '*' : [cb1, cb3]
+ * })
+ *
+ * @param  {Object} routeConfigs  an object containing the path to callbacks pair
+ * @param  {Object} options       path-to-regexp configuration options
+ */
+function multiPageRouter(routeConfigs, options) {
+  var path,
+    callbacks;
+
+  options = options || {};
+
+  for (path in routeConfigs) {
+    if (routeConfigs.hasOwnProperty(path)) {
+      callbacks = routeConfigs[path];
+
+      // ensure that callbacks is an array
+      // by wrapping the single callback if needed
+      if (!isArray(callbacks)) {
+        if ('function' === typeof callbacks) {
+          callbacks = [callbacks];
+        }
+        else {
+
+          // since there is nothing to call just skip
+          // this path
+          continue;
+        }
+      }
+
+      // try to match this path pattern
+      matchPath(path, callbacks, options);
+    }
+  }
+};
+
+/**
+ * Check if a path matches the currentPath and
+ * dispatch a context to the callbacks if
+ * the path matches. Pass an options object
+ * to configure path-to-regexp.
+ *
+ * @param  {string} path      the path string to check window.location against
+ * @param  {Array} callbacks  an array of callbacks to call incase path matches
+ * @param  {Object} options   path-to-regexp configuration options
+ */
+function matchPath(path, callbacks, options) {
+  var keys,
+    key,
+    val,
+    matches,
+    regExp,
+    context,
+    i;
+
+  keys = [];
+  context = {};
+  path = (path === '*') ? '(.*)' : path;
+  regExp = pathToRegexp(path, keys, options);
+  matches = regExp.exec(currentPath);
+
+  if (!matches) return;
+
+  for (i = 1; i < matches.length; i++) {
+    key = keys[i - 1];
+    val = decodeURLEncodedURIComponent(matches[i]);
+
+    if (undefined !== val && !context.hasOwnProperty(key.name)) {
+      context[key.name] = val;
+    }
+  }
+
+  dispatch(callbacks, context);
+};
+
+/**
+ * Dispatch a context to an array of callbacks.
+ *
+ * @param  {Array} callbacks an array containing callback functions
+ * @param  {Object} context   the context object to give to the functions
+ */
+function dispatch(callbacks, context) {
+
+  var idx = 0;
+
+  function _dispatchNext() {
+    var cb = callbacks[idx++],
+      args;
+
+    if ('function' !== typeof cb) return;
+
+    args = [context, _dispatchNext].concat(Array.prototype.slice.call(arguments));
+
+    cb.apply(cb, args);
+  }
+
+  _dispatchNext();
+};
+
+module.exports = multiPageRouter;
